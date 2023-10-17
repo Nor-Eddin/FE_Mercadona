36da9f97a780ae0f10a62fdafd5d99add0209e2d
Nor-Eddin <noreddinlam@gmail.com>
2023-10-17 15:39:16 +0200

    Add fetch & manage the data to Admin Component

--- fe_mercadona/src/Component/Admin.tsx
+++ fe_mercadona/src/Component/Admin.tsx
@@ -1,52 +1,84 @@
+/* eslint-disable prefer-const */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, ButtonGroup } from 'react-bootstrap';
 import './NavMenu.css';
-import ProductsList from '../Products/ProductsList';
+import { Component } from 'react';
+import { urlProduct } from '../endpoints';
 
+export default class Admin extends Component {
+    static displayName = Admin.name;
+    constructor(props: any) {
+        super(props);
+        this.state = { listProducts: [], loading: true };
+    }
+    componentDidMount() {
+        this.listProductsData();
 
-export default function Admin() {
-   
-    return (
-        <>
-            <h1>Gestion des produits</h1>
-            
-            
-            <Button className="btn btn-light ">Ajouter un nouveau produit</Button>
-            <Button className="btn btn-light ">Ajouter une nouvelle categorie</Button>
-            <Button className="btn btn-light ">Ajouter une nouvelle promotion</Button>
+    }
+    componentDidUpdate() {
+        this.listProductsData();
+    }
+    static renderListTable(listProducts: any[]) {
 
-            <table id="tableStyle" className="hover responsive table table-striped" >
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Nom du produit</th>
-                        <th>Descriptions</th>
-                        <th>Prix</th>
-                        <th>Images</th>
-                        <th>Categories</th>
-                        <th>Promotions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row"></th>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                        <td  ><ButtonGroup className="buttonUpdate" >
-                            <Button className="btn btn-light ">Editer</Button>
-                            <Button className="btn btn-light ">Ajouter une promotion</Button>
-                            <Button className="btn btn-danger light">Suprimer</Button>
-                        </ButtonGroup></td>
-                    </tr>
-                </tbody>
+        return (
+            <>
+                {
+                    listProducts.map(product =>
+                        <tr>
+                            <th scope="row">{product.idProduct}</th>
+                            <td>{product.productName}</td>
+                            <td>{product.descriptionProduct}</td>
+                            <td>{product.price}</td>
+                            <td>{product.image}</td>
+                            <td>{product.category}</td>
+                            <td>{product.promotion}</td>
+                            <td  ><ButtonGroup className="buttonUpdate" >
+                                <Button className="btn btn-light ">Editer</Button>
+                                <Button className="btn btn-light ">Ajouter une promotion</Button>
+                                <Button className="btn btn-danger light">Suprimer</Button>
+                            </ButtonGroup></td>
+                        </tr>)
+                }
+            </>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>En chargement...</em></p>
+            : Admin.renderListTable(this.state.listProducts);
+        return (
+            <>
+                <h1>Gestion des produits</h1>
+                <Button className="btn btn-light ">Ajouter un nouveau produit</Button>
+                <Button className="btn btn-light ">Ajouter une nouvelle categorie</Button>
+                <Button className="btn btn-light ">Ajouter une nouvelle promotion</Button>
+                <table id="tableStyle" className="hover responsive table table-striped" >
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Nom du produit</th>
+                            <th>Descriptions</th>
+                            <th>Prix</th>
+                            <th>Images</th>
+                            <th>Categories</th>
+                            <th>Promotions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {contents}
+                    </tbody>
                 </table>
-            
-        </>
-    );
+            </>
+        );
+    }
+    async listProductsData() {
+        const response = await fetch(urlProduct);
+        const data = await response.json();
+        this.setState({ listProducts: data, loading: false });
+
+    }
 }
 
 
