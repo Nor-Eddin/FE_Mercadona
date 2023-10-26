c3e7038e3de017e70eec8014c4bddc1b13fe11eb
Nor-Eddin <noreddinlam@gmail.com>
2023-10-26 18:23:44 +0200

    Add feature delete category

--- fe_mercadona/src/Component/AdminComponent/ConfirmDeleteCategoy.tsx
+++ fe_mercadona/src/Component/AdminComponent/ConfirmDeleteCategoy.tsx
@@ -1,18 +1,19 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import { urlProduct } from '../../endpoints';
+import { urlCategory } from '../../endpoints';
 
-export default function DeleteProduct(props: any) {
+export default function ConfirmDeleteCategory(props: any) {
     const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
+    const handleClose = () => { setShow(false) };
     const handleShow = () => setShow(true);
 
-    async function deleteProduct(id: number) {
+    async function deleteCategory(id: number) {
         try {
-            await fetch(`${urlProduct}/${id}`, { method: 'DELETE' })
+            await fetch(`${urlCategory}/${id}`, { method: 'DELETE' })
                 .then(handleClose);
+
 
         } catch (e) {
             if (e) console.error(e);
@@ -35,11 +36,11 @@ export default function DeleteProduct(props: any) {
                     <Modal.Title>Suppression du produit!!!!</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    Etes vous sur de vouloir supprimer ce produit "{props.productName}"?
+                    Etes vous sur de vouloir supprimer la categorie "{props.catName}"?
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="success" onClick={handleClose}>NON</Button>
-                    <Button variant="danger" onClick={() => deleteProduct(props.idProduct)}>OUI</Button>
+                    <Button variant="danger" onClick={() => deleteCategory(props.catId)}>OUI</Button>
                 </Modal.Footer>
             </Modal>
         </>
